706c6f9b608b873bb70c0d1e05285f70c9806203
xingzhen <xingzhen@moonarstore.com>
2021-06-01 15:19:51 +0800

    mybatis

--- base/src/main/java/com/yzm/base/controller/UserController.java
+++ base/src/main/java/com/yzm/base/controller/UserController.java
@@ -1,11 +1,26 @@
 package com.yzm.base.controller;
 
+import com.yzm.base.entity.User;
+import com.yzm.base.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/list")
+    public void list(){
+        List<User> users = userService.listUser();
+        users.forEach(System.out::println);
+    }
 
 }
 
